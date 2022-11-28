eb7ca66a3b8d4028088a963f735e211d7a8a88ee
Murat Can <muratal0606@gmail.com>
2022-11-29 02:56:13 +0300

    use props to avoid repeat

--- src/pages/Register/Components/BgPic.tsx
+++ src/pages/Register/Components/BgPic.tsx
@@ -1,5 +1,28 @@
-import { Box, Grid, Image, Text } from "@chakra-ui/react";
-import React from "react";
+import { Grid } from "@chakra-ui/react";
+import Card from "./Card";
+
+const data = [
+  {
+    src: "https://d3ptyyxy2at9ui.cloudfront.net/assets/images/ae01c415678c90d0c606bc6917758502.jpg",
+    text1: "30 million+",
+    text2: "app dowloads",
+  },
+  {
+    src: "https://d3ptyyxy2at9ui.cloudfront.net/assets/images/573fa908fbccdf1600f90f16a0c2e311.jpg",
+    text1: "15 years+",
+    text2: "in business ",
+  },
+  {
+    src: "https://d3ptyyxy2at9ui.cloudfront.net/assets/images/87ded242639ba32ef4d0e6c21c9f30aa.jpg",
+    text1: "2 billion+",
+    text2: "tasks completed",
+  },
+  {
+    src: "https://d3ptyyxy2at9ui.cloudfront.net/assets/images/ba90575091d9eafd60f851f7297cb202.jpg",
+    text1: "1 million+",
+    text2: "pro users ",
+  },
+];
 
 const BgPics = () => {
   return (
@@ -9,45 +32,11 @@ const BgPics = () => {
       maxWidth="400"
       maxH="700"
     >
-      <Box padding="1">
-        <Image src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/ae01c415678c90d0c606bc6917758502.jpg" />
-        <Box mt="5" ml="10">
-          <Text as="b" fontSize="sm">
-            30 million+
-          </Text>
-          <Text fontSize="sm">app dowloads </Text>
-        </Box>
-      </Box>
-
-      <Box padding="1">
-        <Image src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/573fa908fbccdf1600f90f16a0c2e311.jpg" />
-        <Box margin="5" ml="10">
-          <Text as="b" fontSize="sm">
-            15 years+
-          </Text>
-          <Text fontSize="sm"> in business </Text>
-        </Box>
-      </Box>
-
-      <Box padding="1">
-        <Image src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/87ded242639ba32ef4d0e6c21c9f30aa.jpg" />
-        <Box margin="5" ml="10">
-          <Text as="b" fontSize="sm">
-            2 billion+
-          </Text>
-          <Text fontSize="sm">tasks completed</Text>
-        </Box>
-      </Box>
-
-      <Box padding="1">
-        <Image src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/ba90575091d9eafd60f851f7297cb202.jpg" />
-        <Box margin="5" ml="10">
-          <Text as="b" fontSize="sm">
-            1 million+
-          </Text>
-          <Text fontSize="sm">pro users </Text>
-        </Box>
-      </Box>
+      <>
+        {data.map((item) => (
+          <Card src={item.src} text1={item.text1} text2={item.text2} />
+        ))}
+      </>
     </Grid>
   );
 };
