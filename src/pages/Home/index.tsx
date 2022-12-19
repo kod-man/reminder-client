82cf88131d12f27d335479ac278a88d0b27ffd59
Murat Can <muratal0606@gmail.com>
2022-12-19 21:22:50 +0300

    add items to navbar

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,18 +5,18 @@ import {
   IconButton,
   Menu,
   MenuButton,
-  Tag,
   Text,
 } from "@chakra-ui/react";
 import { AiOutlineHome } from "react-icons/ai";
-import { BsBell, BsSearch } from "react-icons/bs";
-import { HiPlus } from "react-icons/hi";
+import { BsSearch } from "react-icons/bs";
 import Drawerr from "./Components/Drawer";
+import RgihtSide from "./Components/RightSide";
 
 function HomePage() {
   return (
     <>
       <Flex
+      display="flex"
         direction="row"
         h={10}
         width="100%"
@@ -40,35 +40,7 @@ function HomePage() {
             Search
           </Text>
         </Box>
-        <Flex
-          mt={0}
-          mr={0}
-          flexDirection="row"
-          justifyContent="flex-end"
-          h={10}
-          bg="#db4c3f"
-        >
-          <Box as={Button} bg="#db4c3f" color="white">
-            {<HiPlus />}
-          </Box>
-          <Box as={Button} bg="#db4c3f" color="white">
-            {<BsBell />}
-          </Box>
-          <Tag
-            display="block"
-            m="auto"
-            height={8}
-            width={8}
-            as={Button}
-            bg="white"
-            color="white"
-            border={1}
-            borderRadius="40"
-            borderColor="white"
-          >
-            <Text color="#db4c3f">MC</Text>
-          </Tag>
-        </Flex>
+        <RgihtSide/>
       </Flex>
     </>
   );
