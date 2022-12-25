9350caaee122095eaa29292660fc805705d18afa
ahmet <ahmet55uluisik05@gmail.com>
2022-12-25 22:10:22 +0300

    fix card width

--- src/pages/Onboard/Components/OnboardingCard.tsx
+++ src/pages/Onboard/Components/OnboardingCard.tsx
@@ -1,11 +1,15 @@
-import { Button, Flex, Input, Text } from '@chakra-ui/react';
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
 
 function OnboardingCard() {
   return (
     <>
       <Flex flexDirection="column" alignItems="center" justifyContent="center">
         <Flex alignItems="center" justifyContent="center" h="28" my="10">
-          <Flex flexDirection="column" justifyContent="center" alignItems="center">
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+          >
             <Text fontSize="lg" color="gray.500">
               Daha düzenli olma yolunda ilk adımı attığın için tebrik ederiz.
             </Text>
@@ -14,11 +18,32 @@ function OnboardingCard() {
             </Text>
           </Flex>
         </Flex>
-        <Flex w="30%" minW="200px" h="404px" border="1px" borderColor="gray.200" borderRadius="lg" flexDirection="column" justifyContent="start" alignItems="center">
+        <Flex
+          maxW="300px"
+          w="30%"
+          minW="200px"
+          h="404px"
+          border="1px"
+          borderColor="gray.200"
+          borderRadius="lg"
+          flexDirection="column"
+          justifyContent="start"
+          alignItems="center"
+        >
           <Text as="b" fontSize="lg" mt="6">
             Profilin
           </Text>
-          <Button w="180px" h="180px" borderRadius="100" justifyContent="center" alignItems="center" mt="4" bg="gray.100" color="white" fontSize="100">
+          <Button
+            w="180px"
+            h="180px"
+            borderRadius="100"
+            justifyContent="center"
+            alignItems="center"
+            mt="4"
+            bg="gray.100"
+            color="white"
+            fontSize="100"
+          >
             N
           </Button>
           <Button bg="gray.100" size="sm" mt="4">
@@ -26,10 +51,10 @@ function OnboardingCard() {
           </Button>
           <Flex mt="6">
             <Input
-              _focusVisible={{ boxShadow: 'none', outline: 'none' }}
+              _focusVisible={{ boxShadow: "none", outline: "none" }}
               _placeholder={{
                 opacity: 1,
-                textAlign: 'center',
+                textAlign: "center",
               }}
               placeholder="İsmini ekle"
               size="md"
@@ -46,7 +71,7 @@ function OnboardingCard() {
           color="white"
           backgroundColor="red.200"
           mt="10"
-          _hover={{ cursor: 'not-allowed' }}
+          _hover={{ cursor: "not-allowed" }}
         >
           Todoist'i başlat
         </Button>
