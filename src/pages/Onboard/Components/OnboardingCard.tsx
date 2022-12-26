f0fe508d378bd504bc23ea7a0480d05e4fdccede
ahmet <ahmet55uluisik05@gmail.com>
2022-12-26 17:42:16 +0300

    Added file path to form tags

--- src/pages/Onboard/Components/OnboardingCard.tsx
+++ src/pages/Onboard/Components/OnboardingCard.tsx
@@ -1,69 +1,110 @@
-import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import { Button, Flex, FormLabel, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 
-function OnboardingCard() {
+function Card3() {
   const [name, setName] = useState("");
 
   return (
     <>
       <Flex flexDirection="column" alignItems="center" justifyContent="center">
-        <Flex alignItems="center" justifyContent="center" h="28" my="10">
-          <Flex
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text fontSize="lg" color="gray.500">
-              Daha düzenli olma yolunda ilk adımı attığın için tebrik ederiz.
-            </Text>
-            <Text fontSize="lg" color="gray.500">
-              Sana özel Todoist'in kullanıma hazır.
-            </Text>
-          </Flex>
-        </Flex>
         <Flex
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text fontSize="lg" color="gray.500">
+            Daha düzenli olma yolunda ilk adımı attığın için tebrik ederiz.
+          </Text>
+          <Text fontSize="lg" color="gray.500">
+            Sana özel Todoist'in kullanıma hazır.
+          </Text>
+        </Flex>
+
+        <Flex
+          border="1px"
+          mt="10"
           maxW="300px"
           w="30%"
           minW="200px"
           h="404px"
-          border="1px"
           borderColor="gray.200"
           borderRadius="lg"
           flexDirection="column"
           justifyContent="start"
           alignItems="center"
         >
-          <Text as="b" fontSize="lg" mt="6">
-            Profilin
-          </Text>
-          <Button
-            w="180px"
-            h="180px"
-            borderRadius="100"
-            justifyContent="center"
+          <Flex
+            as="b"
+            fontSize="lg"
+            mt="6"
+            flexDirection="column"
+            justifyContent="start"
             alignItems="center"
-            mt="4"
-            bg="gray.100"
-            color="white"
-            fontSize="100"
           >
-            N
-          </Button>
-          <Button bg="gray.100" size="sm" mt="4">
-            Fotoğraf yükle
-          </Button>
-          <Flex mt="6">
-            <Input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              _focusVisible={{ boxShadow: "none", outline: "none" }}
-              _placeholder={{
-                opacity: 1,
-                textAlign: "center",
-              }}
-              placeholder="İsmini ekle"
-              size="md"
-            />
+            <Flex>Profilin</Flex>
+            <FormLabel htmlFor="upload_image">
+              <Flex
+                w="180px"
+                h="180px"
+                borderRadius="100"
+                justifyContent="center"
+                alignItems="center"
+                mt="4"
+                bg="gray.100"
+                color="white"
+                fontSize="100"
+                typeof="button"
+                flexDirection="column"
+                ml="3"
+                cursor="pointer"
+              >
+                N
+              </Flex>
+              <Input
+                color="white"
+                _focusVisible={{ boxShadow: "none", outline: "none" }}
+                w="48"
+                visibility="hidden"
+                _placeholder={{
+                  opacity: 0.0999,
+                  textAlign: "center",
+                }}
+                type="file"
+                placeholder="İsmini ekle"
+                size="xs"
+                id="upload_image"
+              />
+              <Flex
+                border="1px"
+                borderColor="#EDF2F7"
+                borderRadius="4"
+                typeof="button"
+                bg="#EDF2F7"
+                justifyContent="center"
+                w="130px"
+                h="32px"
+                alignItems="center"
+                ml="10"
+                cursor="pointer"
+              >
+                Fotoğraf yükle
+              </Flex>
+              <Flex mt="6">
+                <Input
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  _focusVisible={{ boxShadow: "none", outline: "none" }}
+                  w="48"
+                  _placeholder={{
+                    opacity: 1,
+                    textAlign: "center",
+                  }}
+                  type="text"
+                  placeholder="İsmini ekle"
+                  size="md"
+                />
+              </Flex>
+            </FormLabel>
           </Flex>
         </Flex>
         <Button
@@ -85,4 +126,4 @@ function OnboardingCard() {
   );
 }
 
-export default OnboardingCard;
+export default Card3;
