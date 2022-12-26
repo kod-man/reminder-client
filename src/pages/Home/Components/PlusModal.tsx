42b7622863fab549b4b3902dbf284fb0336401ab
Murat Can <muratal0606@gmail.com>
2022-12-26 23:42:26 +0300

    add icons on Modal

--- src/pages/Home/Components/PlusModal.tsx
+++ src/pages/Home/Components/PlusModal.tsx
@@ -4,22 +4,47 @@ import {
   Input,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
-  ModalHeader,
   ModalOverlay,
+  Text,
   useDisclosure,
 } from "@chakra-ui/react";
-import { BsPlus } from "react-icons/bs";
+import { BiGitCompare } from "react-icons/bi";
+import { BsAlarm, BsFlag, BsInboxFill, BsPlus } from "react-icons/bs";
+import { MdLabelOutline, MdToday } from "react-icons/md";
 
+const dataModalLeft = [
+  {
+    Icon: BiGitCompare,
+    text: "Today",
+  },
+  {
+    Icon: BsInboxFill,
+    text: "Inbox",
+  },
+];
+const dataModaRight = [
+  {
+    Icon: MdLabelOutline,
+  },
+  {
+    Icon: BsFlag,
+  },
+  {
+    Icon: BsAlarm,
+  },
+  {
+    Icon: MdToday,
+  },
+];
 const PlusModal = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <>
       <Flex
-      bg="#db4c3f"
+        bg="#db4c3f"
         as={Button}
         onClick={onOpen}
         _hover={{ bg: "#e27065" }}
@@ -33,13 +58,55 @@ const PlusModal = () => {
       <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalBody pb={6}>
-          <Input  placeholder='Task name' overflow="hidden"  border="none" fontSize="large" fontFamily="heading" />
-          <Input  placeholder='Description' overflow="hidden"  border="none" fontSize="small" />
+          <ModalBody pb={3}>
+            <Input
+              placeholder="Task name"
+              border="none"
+              fontSize="xl"
+              fontFamily="initial"
+              textColor="#2b2b2b"
+              variant="unstyled"
+            />
+            <Input
+              placeholder="Description"
+              border="none"
+              fontSize="small"
+              variant="unstyled"
+            />
+            <Flex mt={5}>
+              <Button color="green" size="sm">
+                <MdToday />
+                <Text ml={1} fontSize="xs">
+                  Today
+                </Text>
+              </Button>
+              <Button color="blue" size="sm" ml={2}>
+                <BsInboxFill />
+                <Text ml={1} fontSize="xs">
+                  Inbox
+                </Text>
+              </Button>
+              <Flex ml={14}>
+                <Button color="gray" size="sm" ml={2}>
+                  <MdLabelOutline />
+                </Button>
+                <Button color="gray" size="sm" ml={2}>
+                  <BsFlag />
+                </Button>
+                <Button color="gray" size="sm" ml={2}>
+                  <BsAlarm />
+                </Button>
+                <Button color="gray" size="sm" ml={2}>
+                  <BiGitCompare />
+                </Button>
+              </Flex>
+            </Flex>
           </ModalBody>
 
           <ModalFooter>
-            <Button onClick={onClose} size="sm">Cancel</Button>
+            <Button onClick={onClose} size="sm">
+              Cancel
+            </Button>
             <Button colorScheme="red" ml={2} size="sm" disabled>
               Add task
             </Button>
