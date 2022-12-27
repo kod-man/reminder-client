44c667f2cb5020081984ca0e45e7fc2e5fa24a02
ahmet <ahmet55uluisik05@gmail.com>
2022-12-27 17:05:02 +0300

    styling done

--- src/pages/Onboard/Components/OnboardingCard.tsx
+++ src/pages/Onboard/Components/OnboardingCard.tsx
@@ -47,99 +47,90 @@ function Card3() {
           maxW="300px"
           w="30%"
           minW="200px"
-          h="404px"
-          borderColor="blue"
+          h="400px"
+          borderColor="gray.200"
           borderRadius="lg"
           flexDirection="column"
-          justifyContent="start"
+          justifyContent=""
           alignItems="center"
+          textAlign="center"
         >
+          <Text as="b" mt="8" fontSize="large">
+            Profilin
+          </Text>
+
           <Flex
-            border="1px solid red"
-            fontSize="lg"
-            mt="6"
-            flexDirection="column"
-            justifyContent="start"
+            w="180px"
+            h="180px"
+            borderRadius="full"
+            justifyContent="center"
             alignItems="center"
+            mt="4"
+            bg="gray.100"
+            color="white"
+            fontSize="100"
+            typeof="button"
+            flexDirection="column"
+            cursor="pointer"
           >
-            <Text as="b">Profilin</Text>
-            <FormLabel htmlFor="upload_image">
-              <Flex
+            {preview ? (
+              <Image
                 w="180px"
                 h="180px"
-                borderRadius="100"
-                justifyContent="center"
-                alignItems="center"
-                mt="4"
-                bg="gray.100"
-                color="white"
-                fontSize="100"
-                typeof="button"
-                flexDirection="column"
-                // ml="3"
-                cursor="pointer"
-              >
-                {preview ? (
-                  <Image
-                    w="180px"
-                    h="180px"
-                    objectFit="cover"
-                    borderRadius="full"
-                    src={preview}
-                  />
-                ) : (
-                  "N"
-                )}
-              </Flex>
-              <Input
-                color="white"
-                _focusVisible={{ boxShadow: "none", outline: "none" }}
-                w="48"
-                visibility="hidden"
-                _placeholder={{
-                  opacity: 0.0999,
-                  textAlign: "center",
-                }}
-                type="file"
-                placeholder="İsmini ekle"
-                size="xs"
-                id="upload_image"
-                accept="image/png, image/jpg, image/jpeg"
-                onChange={onSelectFile}
+                objectFit="cover"
+                borderRadius="full"
+                src={preview}
               />
+            ) : (
+              "N"
+            )}
+          </Flex>
+          <FormLabel
+            display="flex"
+            alignItems="center"
+            htmlFor="upload_image"
+            flexDirection="column"
+            ml="3"
+            mt="2"
+            maxH="10%"
+          >
+            <Input
+              visibility="hidden"
+              type="file"
+              id="upload_image"
+              accept="image/png, image/jpg, image/jpeg"
+              onChange={onSelectFile}
+            />
 
-              <Flex
-                border="1px"
-                borderColor="#EDF2F7"
-                borderRadius="4"
-                typeof="button"
-                bg="#EDF2F7"
-                justifyContent="center"
-                w="130px"
-                h="32px"
-                alignItems="center"
-                // ml="10"
-                cursor="pointer"
-              >
-                {preview ? "Fotoğraf değiştir" : "Fotoğraf yükle"}
-              </Flex>
-
-              <Flex mt="6">
-                <Input
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  _focusVisible={{ boxShadow: "none", outline: "none" }}
-                  w="48"
-                  _placeholder={{
-                    opacity: 1,
-                    textAlign: "center",
-                  }}
-                  type="text"
-                  placeholder="İsmini ekle"
-                  size="md"
-                />
-              </Flex>
-            </FormLabel>
+            <Flex
+              border="1px"
+              borderColor="#EDF2F7"
+              borderRadius="4"
+              typeof="button"
+              bg="#EDF2F7"
+              justifyContent="center"
+              alignItems="center"
+              cursor="pointer"
+              w="130px"
+            >
+              {preview ? "Fotoğraf değiştir" : "Fotoğraf yükle"}
+            </Flex>
+          </FormLabel>
+          <Flex mt="6">
+            <Input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              _focusVisible={{ boxShadow: "none", outline: "none" }}
+              w="48"
+              _placeholder={{
+                opacity: 1,
+                textAlign: "center",
+              }}
+              type="text"
+              placeholder="İsmini ekle"
+              size="md"
+              mb="8"
+            />
           </Flex>
         </Flex>
         <Button
@@ -151,8 +142,9 @@ function Card3() {
           width="140px"
           border="2px"
           color="white"
-          backgroundColor="red"
+          backgroundColor="red.400"
           mt="10"
+          _hover={{ bg: "#C53030" }}
         >
           Todoist'i başlat
         </Button>
