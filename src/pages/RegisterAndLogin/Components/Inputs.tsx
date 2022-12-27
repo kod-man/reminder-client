69bfd81d4642273e25a1022b3c4918a489abf548
kodman <ufuk.kodman@gmail.com>
2022-12-27 20:12:53 +0100

    add userId to local

--- src/pages/RegisterAndLogin/Components/Inputs.tsx
+++ src/pages/RegisterAndLogin/Components/Inputs.tsx
@@ -7,19 +7,19 @@ import {
   Stack,
   Text,
   useToast,
-} from "@chakra-ui/react";
-import React from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import { useNavigate } from "react-router-dom";
-import { Axios } from "../../../utils/axios";
+} from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { Axios } from '../../../utils/axios';
 import {
   defaultToastProps,
   genericServerToast,
   genericValidationToast,
-} from "../../../utils/genericToast";
-import { PATHS } from "../../../utils/paths";
-import { API } from "../../../utils/usedApi";
-import { InputValidation } from "../utils/InputValidation";
+} from '../../../utils/genericToast';
+import { PATHS } from '../../../utils/paths';
+import { API } from '../../../utils/usedApi';
+import { InputValidation } from '../utils/InputValidation';
 
 const Inputs = ({ page }: { page: string }) => {
   const toast = useToast();
@@ -27,13 +27,13 @@ const Inputs = ({ page }: { page: string }) => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = React.useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const [formErrors, setFormErros] = React.useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   // to make password visible or invisible
@@ -60,15 +60,15 @@ const Inputs = ({ page }: { page: string }) => {
   };
 
   const submitHandler = () => {
-    if (page === "register") {
+    if (page === 'register') {
       Axios.post(API.register, formData)
         .then((res) => {
           console.log(res);
           toast({
             ...defaultToastProps,
-            title: "Account created.",
+            title: 'Account created.',
             description: "We've created your account for you.",
-            status: "success",
+            status: 'success',
           });
           navigate(PATHS.LOGIN);
         })
@@ -83,16 +83,18 @@ const Inputs = ({ page }: { page: string }) => {
         });
     }
     // validation in login and then redirect to the home
-    if (page === "login") {
+    if (page === 'login') {
       Axios.post(API.login, formData)
         .then((res) => {
           toast({
             ...defaultToastProps,
-            title: "Successfully logged in.",
+            title: 'Successfully logged in.',
             description: "You're logged in.",
-            status: "success",
+            status: 'success',
           });
-          localStorage.setItem("token", res.data.response.token);
+          localStorage.setItem('token', res.data.response.token);
+          localStorage.setItem('userId', res.data.response.user.id);
+
           navigate(PATHS.HOME);
           // update local storage
           window.location.reload();
@@ -124,14 +126,14 @@ const Inputs = ({ page }: { page: string }) => {
           />
           {formErrors.email ? (
             <Text color="red" as="i">
-              {formErrors.email}{" "}
+              {formErrors.email}{' '}
             </Text>
           ) : null}
           <InputGroup size="lg">
             <Input
               name="password"
               pr="4.5rem"
-              type={open ? "text" : "password"}
+              type={open ? 'text' : 'password'}
               placeholder="Enter your password..."
               onChange={HandleOnChange}
               value={formData.password}
@@ -165,7 +167,7 @@ const Inputs = ({ page }: { page: string }) => {
         colorScheme="red"
         borderRadius="9"
       >
-        <Text>{page === "register" ? "Sign up with Email" : "Log in"}</Text>
+        <Text>{page === 'register' ? 'Sign up with Email' : 'Log in'}</Text>
       </Button>
     </>
   );
