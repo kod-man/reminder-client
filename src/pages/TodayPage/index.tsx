b2923e96e21658663e4085f1aa5874498aa5a988
ahmet <ahmet55uluisik05@gmail.com>
2022-12-28 21:35:57 +0300

    edit page

--- src/pages/TodayPage/index.tsx
+++ src/pages/TodayPage/index.tsx
@@ -1,6 +1,7 @@
-import { Flex } from '@chakra-ui/react';
-import Header from '../Header';
-import CreateReminder from './CreateReminder';
+import { Flex } from "@chakra-ui/react";
+
+import CreateReminder from "./Components/CreateReminder";
+import Header from "./Components/Header";
 function TodayPage() {
   return (
     <Flex flexDirection="column" alignItems="center" justifyContent="center">
