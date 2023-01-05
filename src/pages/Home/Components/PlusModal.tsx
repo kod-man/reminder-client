28481f75c08de871496c91177d557efa114ed3a9
Murat Can <muratal0606@gmail.com>
2023-01-05 23:09:03 +0300

    remove redundant comments

--- src/pages/Home/Components/PlusModal.tsx
+++ src/pages/Home/Components/PlusModal.tsx
@@ -62,9 +62,9 @@ const PlusModal = () => {
   };
   const userData = {
     title,
-    priority, // O
-    description, //O
-    date: "", //O
+    priority,
+    description,
+    date: "",
     usersId,
   };
   const seeData = () => {
