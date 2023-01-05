c6ecc28e8c3cb1e55e9f6ed95e485e556a95bb1c
ahmet <ahmet55uluisik05@gmail.com>
2023-01-05 22:43:02 +0300

    edit NewCard and  CreateReminder

--- src/pages/TodayPage/Components/CreateReminder.tsx
+++ src/pages/TodayPage/Components/CreateReminder.tsx
@@ -6,6 +6,7 @@ import {
   Spacer,
   Text,
   useMediaQuery,
+  VStack,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import {
@@ -13,9 +14,16 @@ import {
   AiOutlineCalendar,
   AiOutlinePlusCircle,
 } from "react-icons/ai";
+import { BiPencil, BiMessage } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { FaDiceSix } from "react-icons/fa";
 import Centere from "./Center";
+import NewCard from "./NewCard";
 function CreateReminder() {
   const [isAddTaskOpen, setIsAddTaskOpen] = useState(false);
+  const [goster, setGoster] = useState(false);
+  const [gosterFlex, setGosterFlex] = useState(false);
+
   const [taskName, setTaskName] = useState("");
   const [isLargerThan800] = useMediaQuery("(min-width: 800px)");
   const iconLists = ["🏷", "🏳", "⏲", "🧩"];
@@ -51,6 +59,82 @@ function CreateReminder() {
       )}
       {isAddTaskOpen && (
         <Flex direction='column' w={isLargerThan800 ? "60%" : "80%"} mt='4'>
+          {gosterFlex && (
+            <Flex w='100%'>
+              {!goster && (
+                <Flex
+                  justifyContent='center'
+                  alignItems='center'
+                  mr='3'
+                  mt='-8'
+                >
+                  <FaDiceSix color='gray' />
+                </Flex>
+              )}
+              <Flex
+                borderBottom='1px solid'
+                borderColor='gray.200'
+                w='100%'
+                h='80px'
+                mt='8'
+                cursor='pointer'
+                onMouseUp={() => setGoster(!goster)}
+              >
+                <Flex>
+                  <Flex
+                    border='1px solid gray'
+                    borderRadius='100%'
+                    w='20px'
+                    h='20px'
+                  ></Flex>
+                  <Flex ml='2' direction='column'>
+                    <Text>Random</Text>
+                    <Text fontSize='sm' color='gray'>
+                      abc
+                    </Text>
+                  </Flex>
+                </Flex>
+                <Spacer />
+                <VStack spacing={4} align='stretch'>
+                  {!goster && (
+                    <Flex>
+                      <Text fontSize='2xl' mr='3' _hover={{ bg: "gray.200" }}>
+                        <BiPencil />
+                      </Text>
+                      <Text fontSize='2xl' mr='3' _hover={{ bg: "gray.200" }}>
+                        <AiOutlineCalendar />
+                      </Text>
+                      <Text fontSize='2xl' _hover={{ bg: "gray.200" }}>
+                        <BiMessage />
+                      </Text>
+                    </Flex>
+                  )}
+                  <Flex
+                    h='30px'
+                    justifyContent='flex-end'
+                    alignItems='flex-end'
+                  >
+                    <Text color='blackAlpha.700' mr='1' fontSize='sm'>
+                      Klasör
+                    </Text>
+                    <AiFillFolderOpen />
+                  </Flex>
+                </VStack>
+              </Flex>
+
+              <Flex
+                h='30px'
+                justifyContent='center'
+                alignItems='center'
+                ml='3'
+                mt='6'
+                _hover={{ bg: "gray.200" }}
+                display='none'
+              >
+                <BsThreeDots />
+              </Flex>
+            </Flex>
+          )}
           <Box
             alignItems='center'
             justifyContent='center'
@@ -147,6 +231,7 @@ function CreateReminder() {
               bg={!taskName.trim() ? "red.300" : "red.500"}
               disabled={!taskName.trim()}
               _hover={!taskName ? { bg: "" } : { bg: "red.700" }}
+              onClick={() => setGosterFlex(!gosterFlex)}
             >
               Görev ekle
             </Button>
--- src/pages/TodayPage/Components/NewCard.tsx
+++ src/pages/TodayPage/Components/NewCard.tsx
@@ -1,51 +1,77 @@
 import { Text, Flex, Spacer, VStack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { AiFillFolderOpen, AiOutlineCalendar } from "react-icons/ai";
 import { BiMessage, BiPencil } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { FaDiceSix } from "react-icons/fa";
 
 function NewCard() {
+  const [goster, setGoster] = useState(false);
   return (
-    <Flex
-      borderBottom='1px solid'
-      borderColor='gray.200'
-      w='60%'
-      h='80px'
-      mt='8'
-    >
-      <Flex>
-        <Flex
-          border='1px solid gray'
-          borderRadius='100%'
-          w='20px'
-          h='20px'
-        ></Flex>
-        <Flex ml='2' direction='column'>
-          <Text>Random</Text>
-          <Text fontSize='sm' color='gray'>
-            abc
-          </Text>
+    <Flex w='100%'>
+      {!goster && (
+        <Flex justifyContent='center' alignItems='center' mr='3' mt='-8'>
+          <FaDiceSix color='gray' />
         </Flex>
-      </Flex>
-      <Spacer />
-      <VStack spacing={4} align='stretch'>
+      )}
+      <Flex
+        borderBottom='1px solid'
+        borderColor='gray.200'
+        w='100%'
+        h='80px'
+        mt='8'
+        cursor='pointer'
+        onMouseUp={() => setGoster(!goster)}
+      >
         <Flex>
-          <Text fontSize='2xl' mr='3'>
-            <BiPencil />
-          </Text>
-          <Text fontSize='2xl' mr='3'>
-            <AiOutlineCalendar />
-          </Text>
-          <Text fontSize='2xl'>
-            <BiMessage />
-          </Text>
+          <Flex
+            border='1px solid gray'
+            borderRadius='100%'
+            w='20px'
+            h='20px'
+          ></Flex>
+          <Flex ml='2' direction='column'>
+            <Text>Random</Text>
+            <Text fontSize='sm' color='gray'>
+              abc
+            </Text>
+          </Flex>
         </Flex>
-        <Flex h='30px' justifyContent='flex-end' alignItems='flex-end'>
-          <Text color='blackAlpha.700' mr='1' fontSize='sm'>
-            Klasör
-          </Text>
-          <AiFillFolderOpen />
-        </Flex>
-      </VStack>
+        <Spacer />
+        <VStack spacing={4} align='stretch'>
+          {!goster && (
+            <Flex>
+              <Text fontSize='2xl' mr='3' _hover={{ bg: "gray.200" }}>
+                <BiPencil />
+              </Text>
+              <Text fontSize='2xl' mr='3' _hover={{ bg: "gray.200" }}>
+                <AiOutlineCalendar />
+              </Text>
+              <Text fontSize='2xl' _hover={{ bg: "gray.200" }}>
+                <BiMessage />
+              </Text>
+            </Flex>
+          )}
+          <Flex h='30px' justifyContent='flex-end' alignItems='flex-end'>
+            <Text color='blackAlpha.700' mr='1' fontSize='sm'>
+              Klasör
+            </Text>
+            <AiFillFolderOpen />
+          </Flex>
+        </VStack>
+      </Flex>
+
+      <Flex
+        h='30px'
+        justifyContent='center'
+        alignItems='center'
+        ml='3'
+        mt='6'
+        _hover={{ bg: "gray.200" }}
+        display='none'
+      >
+        <BsThreeDots />
+      </Flex>
     </Flex>
   );
 }
