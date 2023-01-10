698470b1a2c301d85c4e69eb2012799dac586975
Murat Can <muratal0606@gmail.com>
2023-01-10 16:08:50 +0300

    fix profile menu items

--- src/pages/Home/Components/ProfileCards.tsx
+++ src/pages/Home/Components/ProfileCards.tsx
@@ -21,14 +21,21 @@ const ProfileCards: FC<CardProps> = ({ Icon, text }) => {
           borderRadius: "5px",
         }}
         px={2}
-        h="24"
-        w="24"
+        w="100%"
         justifyContent="flex-start"
         alignItems="center"
         mb={1}
         p="1"
       >
-        <Box h={4} w={4} mr="1" justifyContent="center" alignItems="center" textAlign="left" padding="5px -10px" >
+        <Box
+          h={4}
+          w={4}
+          mr="1"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="left"
+          padding="5px -10px"
+        >
           <Icon color="gray" />
         </Box>
         <Text fontSize=".81rem" ml="2" mt={1}>
