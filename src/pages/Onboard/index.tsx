5592f58b8c3cd9b7697f1d661aa1977dd203ac5e
Murat Can <muratal0606@gmail.com>
2023-01-13 23:50:03 +0300

    remove unused imports

--- src/pages/Onboard/index.tsx
+++ src/pages/Onboard/index.tsx
@@ -1,36 +1,39 @@
-import { Flex, Toast, useToast, } from '@chakra-ui/react';
-import { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
-import { Axios } from '../../utils/axios';
-import { genericServerToast, genericValidationToast } from '../../utils/genericToast';
-import { PATHS } from '../../utils/paths';
-import { API } from '../../utils/usedApi';
-import Todoist from '../RegisterAndLogin/Components/Todoist';
-import OnboardingCard from './Components/OnboardingCard';
+import { Flex, useToast } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Axios } from "../../utils/axios";
+import {
+  genericServerToast,
+  genericValidationToast,
+} from "../../utils/genericToast";
+import { PATHS } from "../../utils/paths";
+import { API } from "../../utils/usedApi";
+import Todoist from "../RegisterAndLogin/Components/Todoist";
+import OnboardingCard from "./Components/OnboardingCard";
 
 function Onboard() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const toast = useToast();
   const userId = sessionStorage.getItem("userId");
 
   useEffect(() => {
     Axios.get(`${API.getUser}/${userId}`)
-    .then(response => {
-      if(response.data.user.userName){
-        navigate(PATHS.HOME)
-      }
-      console.log(response)
-    })
-    .catch((err) => {
-      if (err.response) {
-        console.log(err.response.data.message);
-        genericValidationToast(toast, err);
-      } else {
-        console.log(err);
-        genericServerToast(toast);
-      }
-    });
-  },[userId]);
+      .then((response) => {
+        if (response.data.user.userName) {
+          navigate(PATHS.HOME);
+        }
+        console.log(response);
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.data.message);
+          genericValidationToast(toast, err);
+        } else {
+          console.log(err);
+          genericServerToast(toast);
+        }
+      });
+  }, [userId]);
 
   return (
     <>
@@ -45,4 +48,3 @@ function Onboard() {
 }
 
 export default Onboard;
-
