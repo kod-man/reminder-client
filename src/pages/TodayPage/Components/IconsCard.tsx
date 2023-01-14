dfcd551b931cd6eee95fb67b63162d97d32ca505
ahmet <ahmet55uluisik05@gmail.com>
2023-01-14 11:50:11 +0300

    create ıcons

--- src/pages/TodayPage/Components/IconsCard.tsx
+++ src/pages/TodayPage/Components/IconsCard.tsx
@@ -1,5 +1,9 @@
 import { Flex, Text } from "@chakra-ui/react";
-const iconLists = ["🏷", "🏳", "⏲", "🧩"];
+import AlarmIcon from "../icons/AlarmIcon";
+import FlagIcon from "../icons/FlagIcon";
+import JigsawIcon from "../icons/JigsawIcon";
+import TicketIcon from "../icons/TicketIcon";
+const iconLists = [<TicketIcon />, <FlagIcon />, <AlarmIcon />, <JigsawIcon />];
 function IconsCard() {
   return (
     <Flex alignItems='flex-end' justifyContent='flex-end' w='200px' mb='4'>
