6a849d7860645138aa11d39e391b33825864a9cd
ahmet <ahmet55uluisik05@gmail.com>
2023-01-14 12:18:08 +0300

    design reminderCard

--- src/pages/TodayPage/Components/IconsBar.tsx
+++ src/pages/TodayPage/Components/IconsBar.tsx
@@ -1,4 +1,4 @@
-import { VStack, Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import DateIcon from "../icons/DateIcon";
 import FolderIcon from "../icons/FolderIcon";
 import MessageIcon from "../icons/MessageIcon";
@@ -6,15 +6,15 @@ import PenIcon from "../icons/PenIcon";
 const icons = [<PenIcon />, <DateIcon />, <MessageIcon />];
 function IconsBar() {
   return (
-    <VStack spacing={4} align='stretch'>
-      <Flex>
+    <Flex align='stretch' direction='column'>
+      <Flex justifyContent='flex-start'>
         {icons.map((icon2, index) => (
-          <Text key={index} fontSize='2xl' mr='3' _hover={{ bg: "gray.200" }}>
+          <Text key={index} fontSize='2xl' ml='3' _hover={{ bg: "gray.200" }}>
             {icon2}
           </Text>
         ))}
       </Flex>
-      <Flex h='30px' justifyContent='flex-end' alignItems='flex-end'>
+      <Flex justifyContent='flex-end' alignItems='flex-end'>
         <Text color='blackAlpha.700' mr='1' fontSize='sm'>
           Klasör
         </Text>
@@ -22,7 +22,7 @@ function IconsBar() {
           <FolderIcon color='blue' />
         </Flex>
       </Flex>
-    </VStack>
+    </Flex>
   );
 }
 
--- src/pages/TodayPage/Components/ReminderCard.tsx
+++ src/pages/TodayPage/Components/ReminderCard.tsx
@@ -1,5 +1,5 @@
 import { Flex, Input, Spacer, useMediaQuery } from "@chakra-ui/react";
-import CircleIcon from "../icons/CircleIcon";
+
 import TreeDoteIcon from "../icons/TreeDoteIcon";
 import IconsBar from "./IconsBar";
 
@@ -16,21 +16,17 @@ function ReminderCard({ title, description }: ReminderCardProps) {
         borderBottom='1px solid'
         borderColor='gray.200'
         w='100%'
-        h='80px'
+        h='50px'
         cursor='pointer'
       >
         <Flex>
           <Flex
             border='1px solid gray'
             borderRadius='50%'
-            w='16px'
-            h='16px'
-            alignItems='center'
-            justifyContent='center'
-            overflow='hidden'
-          >
-            <CircleIcon />
-          </Flex>
+            h='20px'
+            w='20px'
+            mt='1'
+          ></Flex>
           <Flex ml='2' direction='column'>
             <Input variant='unstyled' value={title} name='title' />
             <Input
@@ -51,6 +47,7 @@ function ReminderCard({ title, description }: ReminderCardProps) {
         _hover={{ bg: "gray.200" }}
         alignItems='center'
         justifyContent='center'
+        ml='2'
       >
         <TreeDoteIcon color='gray' />
       </Flex>
