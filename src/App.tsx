5c838a76882b37e4336a55c1682a4e5f3c8033f0
Murat Can <muratal0606@gmail.com>
2023-01-26 15:00:27 +0300

    create a constant value for protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,38 @@
-import { Route, Routes } from 'react-router-dom';
-import ProtectedRoutes from './components/ProtectedRoutes';
-import HomePage from './pages/Home';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
-import Onboard from './pages/Onboard';
-import RegisterAndLogin from './pages/RegisterAndLogin';
-import TodayPage from './pages/TodayPage';
-import { PATHS } from './utils/paths';
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import HomePage from "./pages/Home";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Onboard from "./pages/Onboard";
+import RegisterAndLogin from "./pages/RegisterAndLogin";
+import TodayPage from "./pages/TodayPage";
+import { PATHS } from "./utils/paths";
 
 export const App = () => {
-  const token = sessionStorage.getItem('token');
+  const token = sessionStorage.getItem("token");
+  const protectedRoutes = [
+    { path: PATHS.HOME, component: <HomePage /> },
+    { path: PATHS.NOT_FOUND, component: <NotFoundPage /> },
+    { path: PATHS.ONBOARD, component: <Onboard /> },
+    { path: PATHS.TODAY, component: <TodayPage /> },
+  ];
 
+  const Pages = protectedRoutes.map((item) => (
+    <Route
+      key={item.path}
+      path={item.path}
+      element={
+        <ProtectedRoutes token={token}>{item.component}</ProtectedRoutes>
+      }
+    />
+  ));
   return (
     <Routes>
-      <Route path={PATHS.REGISTER} element={<RegisterAndLogin page="register" />} />
+      <Route
+        path={PATHS.REGISTER}
+        element={<RegisterAndLogin page="register" />}
+      />
       <Route path={PATHS.LOGIN} element={<RegisterAndLogin page="login" />} />
-      <Route
-        path={PATHS.HOME}
-        element={
-          <ProtectedRoutes token={token}>
-            <HomePage />
-          </ProtectedRoutes>
-        }
-      />
-      <Route
-        path={PATHS.NOT_FOUND}
-        element={
-          <ProtectedRoutes token={token}>
-            <NotFoundPage />
-          </ProtectedRoutes>
-        }
-      />
-      <Route
-        path={PATHS.ONBOARD}
-        element={
-          <ProtectedRoutes token={token}>
-            <Onboard />
-          </ProtectedRoutes>
-        }
-      />
-
-      <Route
-        path={PATHS.TODAY}
-        element={
-          <ProtectedRoutes token={token}>
-            <TodayPage />
-          </ProtectedRoutes>
-        }
-      />
+      {Pages}
     </Routes>
   );
 };
