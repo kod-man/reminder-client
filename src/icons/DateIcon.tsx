5d5e33db39196186a4b3ce9aa7c867aafef20c91
Murat Can <muratal0606@gmail.com>
2023-01-28 01:58:21 +0300

    fix icons typo

--- src/icons/DateIcon.tsx
+++ src/icons/DateIcon.tsx
@@ -11,8 +11,8 @@ function DateIcon(props: any) {
       {...props}
     >
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2zM5 6a1 1 0 011-1h12a1 1 0 011 1v12a1 1 0 01-1 1H6a1 1 0 01-1-1V6zm12 10a1 1 0 11-2 0 1 1 0 012 0zM7 8a.5.5 0 000 1h10a.5.5 0 000-1H7z'
         fill='currentColor'
       ></path>
