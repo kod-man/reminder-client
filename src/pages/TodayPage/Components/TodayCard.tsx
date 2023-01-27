ce83322a31230da567957bcc06466eb4ecc6180d
ahmet <ahmet55uluisik05@gmail.com>
2023-01-27 20:56:28 +0300

    create priotry and reminders

--- src/pages/TodayPage/Components/TodayCard.tsx
+++ src/pages/TodayPage/Components/TodayCard.tsx
@@ -1,51 +1,81 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { useState } from "react";
-import FolderIcon from "../../../icons/FolderIcon";
+import ClockIcon from "../../../icons/ClockIcon";
+import Flag2Icon from "../../../icons/Flag2Icon";
 import TodayIcon from "../../../icons/TodayIcon";
+import TreeDoteIcon from "../../../icons/TreeDoteIcon";
 import TodoCalendar from "./TodoCalendar ";
 
 function TodayCard() {
   const [showCard, setShowCard] = useState(false);
   return (
     <>
-      <Flex ml="3" mt="2">
+      <Flex ml='3' mt='2'>
         <Flex
           onClick={() => {
             setShowCard(!showCard);
           }}
-          border="1px"
-          borderColor="gray.300"
-          color="green"
-          p="2"
-          mr="2"
-          borderRadius="md"
-          w="80px"
-          h="30px"
-          alignItems="center"
-          justifyContent="center"
-          cursor="pointer"
+          border='1px'
+          borderColor='gray.300'
+          color='green'
+          p='2'
+          mr='2'
+          borderRadius='md'
+          h='30px'
+          alignItems='center'
+          justifyContent='center'
+          cursor='pointer'
           _hover={{ bg: "gray.200" }}
         >
           <TodayIcon />
-          <Text ml="1">Today</Text>
+          <Text ml='1'>Today</Text>
         </Flex>
         <Flex
-          color="blue.400"
-          border="1px"
-          borderColor="gray.300"
-          borderRadius="md"
-          p="2"
-          w="80px"
-          h="30px"
-          alignItems="center"
-          justifyContent="center"
-          cursor="pointer"
+          mr='2'
+          border='1px'
+          borderColor='gray.300'
+          borderRadius='md'
+          p='2'
+          h='30px'
+          alignItems='center'
+          justifyContent='center'
+          cursor='pointer'
           _hover={{ bg: "gray.200" }}
         >
-          <FolderIcon color="blue.500" />
-          <Text color="blackAlpha.700" ml="1">
-            Inbox
+          <Flag2Icon />
+          <Text color='gray' ml='1'>
+            Priority
           </Text>
+        </Flex>
+        <Flex
+          cursor='pointer'
+          mr='2'
+          border='1px'
+          borderColor='red'
+          borderRadius='md'
+          p='2'
+          h='30px'
+          alignItems='center'
+          justifyContent='center'
+          _hover={{ bg: "gray.200" }}
+        >
+          <ClockIcon />
+          <Text color='gray' ml='1'>
+            Reminders
+          </Text>
+        </Flex>
+        <Flex
+          border='1px'
+          borderColor='gray.300'
+          borderRadius='md'
+          p='2'
+          h='30px'
+          alignItems='center'
+          justifyContent='center'
+          cursor='pointer'
+          _hover={{ bg: "gray.200" }}
+        >
+          <TreeDoteIcon />
         </Flex>
       </Flex>
       {showCard ? <TodoCalendar /> : null}
