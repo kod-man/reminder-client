f9fbe38d257f82f2ae2324f69a43032d3513b170
Murat Can <muratal0606@gmail.com>
2023-01-31 01:33:37 +0300

    add TabPanel2

--- src/components/Navbar/Components/Notification.tsx
+++ src/components/Navbar/Components/Notification.tsx
@@ -31,9 +31,7 @@ const Notification = () => {
       >
         <BellIcon color="white" />
       </Flex>
-
       <MenuList>
-        {/* colorScheme='white'  didint worked  */}
         <Tabs variant="soft-rounded" colorScheme="whiteAlpha">
           <Flex justifyContent="space-between" alignItems="center" p={2}>
             <TabList backgroundColor="gray.400" borderRadius={20}>
@@ -60,7 +58,11 @@ const Notification = () => {
           </Flex>
           <TabPanels>
             <TabPanel w="550px">
-              <Flex box-sizing="border-box" justifyContent="center" margin="auto">
+              <Flex
+                box-sizing="border-box"
+                justifyContent="center"
+                margin="auto"
+              >
                 <Flex mt="10px" justifyContent="center">
                   <Image
                     src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/14ddf84cccca3f09cad7245007a41e09.jpg"
@@ -73,14 +75,23 @@ const Notification = () => {
                 <Text color="black" fontWeight="medium">
                   Stay in the loop
                 </Text>
-                <Text color="#202020" fontSize="sm">
-                  Here, you’ll find notifications for any changes that happen in your shared
-                  projects.
+                <Text color="gray" fontSize="sm">
+                  Here, you’ll find notifications for any changes that happen in
+                  your shared projects.
                 </Text>
-                <HStack alignItems="center" justifyContent="center" marginTop="10px">
+                <HStack
+                  alignItems="center"
+                  justifyContent="center"
+                  marginTop="10px"
+                >
                   <Flex mt="15px" alignItems="center" justifyContent="center">
                     <QuestionMarkIcon color="#d1453b" />
-                    <Link color="#d1453b" fontSize="sm" ml="5px" textDecoration="underline">
+                    <Link
+                      color="#d1453b"
+                      fontSize="sm"
+                      ml="5px"
+                      textDecoration="underline"
+                    >
                       How to share projects
                     </Link>
                   </Flex>
@@ -88,7 +99,23 @@ const Notification = () => {
               </Stack>
             </TabPanel>
             <TabPanel w="550px">
-              <p>two!</p>
+              <Flex
+                box-sizing="border-box"
+                justifyContent="center"
+                margin="auto"
+              >
+                <Flex mt="10px" justifyContent="center">
+                  <Image
+                    src="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/14ddf84cccca3f09cad7245007a41e09.jpg"
+                    srcSet="https://d3ptyyxy2at9ui.cloudfront.net/assets/images/dfd21d376f6b295a42b0e57c810fba7f.jpg 2x, https://d3ptyyxy2at9ui.cloudfront.net/assets/images/f553f26a1275e2d1ea579ab575e7d5ab.jpg 3x"
+                  />
+                </Flex>
+              </Flex>
+              <Flex alignItems="center" justifyContent="center">
+                <Text color="gray" fontSize="sm">
+                  Nice work! You’re all caught up.
+                </Text>
+              </Flex>
             </TabPanel>
           </TabPanels>
         </Tabs>
