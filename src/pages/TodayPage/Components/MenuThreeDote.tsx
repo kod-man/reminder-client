216f06e1dbaa5341550152d18e0c9774a580345d
kodman <ufuk.kodman@gmail.com>
2023-02-01 22:14:08 +0100

    fix divider issue

--- src/pages/TodayPage/Components/MenuThreeDote.tsx
+++ src/pages/TodayPage/Components/MenuThreeDote.tsx
@@ -1,8 +1,8 @@
 import {
   Button,
+  Divider,
   Menu,
   MenuButton,
-  MenuDivider,
   MenuItem,
   MenuList,
   Spacer,
@@ -17,16 +17,18 @@ const menuItemsThreeDote = [
     icon: <TicketIcon />,
     text: "Labels",
     sign: "@",
+    hasDivider: false,
   },
   {
     icon: <ArrowIcon />,
     text: "Move to project",
     sign: "#",
-    hr: <MenuDivider />,
+    hasDivider: true,
   },
   {
     icon: <JigsawIcon />,
     text: "Add extension...",
+    hasDivider: true,
   },
 ];
 
@@ -35,38 +37,34 @@ function MenuThreeDote() {
     <Menu>
       <MenuButton
         as={Button}
-        bg='white'
-        border='1px'
-        borderColor='gray.300'
-        borderRadius='md'
-        alignItems='center'
-        justifyContent='center'
-        cursor='pointer'
-        h='32px'
-        p='0'
-        minWidth='32px'
+        bg="white"
+        border="1px"
+        borderColor="gray.300"
+        borderRadius="md"
+        alignItems="center"
+        justifyContent="center"
+        cursor="pointer"
+        h="32px"
+        p="0"
+        minWidth="32px"
       >
-        <Text
-          display='flex'
-          justifyContent='center'
-          fontSize='2xl'
-          mb='3'
-          color='gray'
-        >
+        <Text display="flex" justifyContent="center" fontSize="2xl" mb="3" color="gray">
           ...
         </Text>
       </MenuButton>
       <MenuList>
         {menuItemsThreeDote.map((item) => (
-          <MenuItem key={item.text}>
-            {item.icon}
-            <Text ml='2'>{item.text}</Text>
-            <Spacer />
-            {item.sign}
-          </MenuItem>
+          <>
+            <MenuItem key={item.text}>
+              {item.icon}
+              <Text ml="2">{item.text}</Text>
+              <Spacer />
+              {item.sign ? item.sign : null}
+            </MenuItem>
+            {item.hasDivider ? <Divider /> : null}
+          </>
         ))}
-        <MenuDivider />
-        <MenuItem color='red'>Edit task actions</MenuItem>
+        <MenuItem color="red">Edit task actions</MenuItem>
       </MenuList>
     </Menu>
   );
