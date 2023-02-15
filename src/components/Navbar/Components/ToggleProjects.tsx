f809321c5c32fa5b718f3d852c41622f1dd67201
Murat Can <muratal0606@gmail.com>
2023-02-16 00:52:24 +0300

    remove flex

--- src/components/Navbar/Components/ToggleProjects.tsx
+++ src/components/Navbar/Components/ToggleProjects.tsx
@@ -13,34 +13,25 @@ import MyTooltip from "./MyTooltip";
 
 const ToggleProjects = () => {
   return (
-    <Flex
-      style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "flex-end",
-        height: "100%",
-      }}
-    >
-      <Accordion allowMultiple>
-        <AccordionItem border="none">
-          <MyTooltip label="Toggle list of Projects">
-            <AccordionButton
-              aria-expanded="true"
-              style={{ backgroundColor: "transparent" }}
-            >
-              <Box as="span" flex="1" textAlign="left"></Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </MyTooltip>
-          <AccordionPanel pb={4}>
-            <Flex justifyContent="center" alignItems="center">
-              <ColorDotIcon color="gray" />
-              <Text>Personal 🙂 </Text>
-            </Flex>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </Flex>
+    <Accordion allowMultiple>
+      <AccordionItem border="none">
+        <MyTooltip label="Toggle list of Projects">
+          <AccordionButton
+            aria-expanded="true"
+            style={{ backgroundColor: "transparent" }}
+          >
+            <Box as="span" flex="1" textAlign="left"></Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </MyTooltip>
+        <AccordionPanel pb={4}>
+          <Flex justifyContent="center" alignItems="center">
+            <ColorDotIcon color="gray" />
+            <Text>Personal 🙂 </Text>
+          </Flex>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
