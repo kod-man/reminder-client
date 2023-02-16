2e87ca78cc258a9cdc175197da88b25983a4baec
Murat Can <muratal0606@gmail.com>
2023-02-16 19:39:51 +0300

    remove acordion component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import LeftSideBar from "./Components/LeftNavBar";
 import RightNavBar from "./Components/RightNavBar";
-import ToggleProjects from "./Components/ToggleProjects";
 
 function Navbar() {
   return (
-    <Flex h="44px" bg="#db4c3f" alignItems="center" justifyContent="center" py={2}>
+    <Flex
+      h="44px"
+      bg="#db4c3f"
+      alignItems="center"
+      justifyContent="center"
+      py={2}
+    >
       <LeftSideBar />
       <RightNavBar />
     </Flex>
