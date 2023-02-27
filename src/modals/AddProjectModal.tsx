b6c8ab0d6d6d2ec217815a4514167f2d8b2d8a38
ahmet <ahmet55uluisik05@gmail.com>
2023-02-27 21:59:36 +0300

    edit toast message

--- src/modals/AddProjectModal.tsx
+++ src/modals/AddProjectModal.tsx
@@ -37,7 +37,9 @@ const AddProjectModal = () => {
     setColor(event.target.value);
   };
 
-  const handleNameChange = (event: { target: { value: React.SetStateAction<string> } }) => {
+  const handleNameChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
     setName(event.target.value);
   };
 
@@ -60,7 +62,7 @@ const AddProjectModal = () => {
       .then(() => {
         toast({
           ...defaultToastProps,
-          title: "Project added.",
+          title: "Project added successfully.",
           status: "success",
         });
       })
@@ -71,39 +73,44 @@ const AddProjectModal = () => {
 
   return (
     <>
-      <MyTooltip label="Add project">
+      <MyTooltip label='Add project'>
         <Flex>
           <SmallPlusIcon onClick={onOpen} />
         </Flex>
       </MyTooltip>
-      <Modal finalFocusRef={finalRef} isOpen={isOpen} onClose={onClose} size="md">
+      <Modal
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+        size='md'
+      >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader position="relative" fontWeight="bold" fontSize="20px">
+          <ModalHeader position='relative' fontWeight='bold' fontSize='20px'>
             <Text>Add project</Text>
             <Flex
-              position="absolute"
-              top="1px"
-              right="2px"
-              as="button"
-              border="none"
-              m="10px 10px 0 0"
+              position='absolute'
+              top='1px'
+              right='2px'
+              as='button'
+              border='none'
+              m='10px 10px 0 0'
             >
-              <QuestionMarkIcon color="black" />
+              <QuestionMarkIcon color='black' />
             </Flex>
           </ModalHeader>
 
           <Divider />
           <ModalBody>
-            <Flex flexDir="column">
-              <Text fontWeight="bold" m="8px 0 5px 0" fontSize="14px">
+            <Flex flexDir='column'>
+              <Text fontWeight='bold' m='8px 0 5px 0' fontSize='14px'>
                 Name
               </Text>
               <Input
-                borderRadius="7px"
-                border="1px"
-                borderColor="gray"
-                outline="none"
+                borderRadius='7px'
+                border='1px'
+                borderColor='gray'
+                outline='none'
                 _focus={{
                   borderColor: "gray",
                   boxShadow: "none",
@@ -113,34 +120,43 @@ const AddProjectModal = () => {
                 onChange={handleNameChange}
               />
             </Flex>
-            <Flex flexDirection="column" mt="12px">
-              <Text fontWeight="bold" m="8px 0 5px 0" fontSize="14px">
+            <Flex flexDirection='column' mt='12px'>
+              <Text fontWeight='bold' m='8px 0 5px 0' fontSize='14px'>
                 Color
               </Text>
-              <CustomSelects color={color} handleColorChange={handleColorChange} />
+              <CustomSelects
+                color={color}
+                handleColorChange={handleColorChange}
+              />
             </Flex>
-            <Flex alignItems="center" mt="15px">
+            <Flex alignItems='center' mt='15px'>
               <Switch
-                colorScheme="teal"
+                colorScheme='teal'
                 isChecked={isFavorite}
                 onChange={() => setIsFavorite(!isFavorite)}
               />
-              <Text ml="10px" fontSize="14px">
+              <Text ml='10px' fontSize='14px'>
                 Add to favorites
               </Text>
             </Flex>
           </ModalBody>
-          <Divider mt="5px" />
+          <Divider mt='5px' />
           <ModalFooter>
-            <Button colorScheme="gray" mr={3} onClick={onClose} width="70px" height="35px">
+            <Button
+              colorScheme='gray'
+              mr={3}
+              onClick={onClose}
+              width='70px'
+              height='35px'
+            >
               Cancel
             </Button>
             <Button
-              variant="ghost"
-              backgroundColor="#f1b7b2"
-              width="70px"
-              height="35px"
-              textColor="white"
+              variant='ghost'
+              backgroundColor='#f1b7b2'
+              width='70px'
+              height='35px'
+              textColor='white'
               disabled={name.trim() === ""}
               onClick={submitHandler}
               _hover={{ backgroundColor: "#c0392b!important" }}
