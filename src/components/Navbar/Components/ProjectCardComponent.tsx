7569a2d32b9f48d180d9eb4064faa87dd16ed37f
Murat Can <muratal0606@gmail.com>
2023-03-01 19:47:20 +0300

    get request with userId

--- src/components/Navbar/Components/ProjectCardComponent.tsx
+++ src/components/Navbar/Components/ProjectCardComponent.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import ColorDotIcon from "../../../icons/ColorDotIcon";
 import { Axios } from "../../../utils/axios";
 import { API } from "../../../utils/usedApi";
@@ -9,15 +10,19 @@ type ProjectCardComponentProps = {
 };
 
 function ProjectCardComponent({ name, color }: ProjectCardComponentProps) {
+  const userId = sessionStorage.getItem("userId");
+  // const [projects, setProjects] = useState({});
+ 
 
-  Axios.get(API.addProject)
-  .then(() => {
-    console.log(Response)
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  Axios.get(`${API.addProject}/${userId}`)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
+    
   return (
     <Flex
       m="-5px 10px 0 25px "
