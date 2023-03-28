5fa35a317e65cb9b60597ec366aba9ef6db6f130
Murat Can <muratal0606@gmail.com>
2023-03-29 01:10:43 +0300

    add data

--- src/components/Navbar/Components/ThreeDotComponent.tsx
+++ src/components/Navbar/Components/ThreeDotComponent.tsx
@@ -11,6 +11,16 @@ import {
   useDisclosure
 } from "@chakra-ui/react";
 import { ReactNode } from "react";
+import AboveIcon from "../../../icons/AboveIcon";
+import ArchiveIcon from "../../../icons/ArchiveIcon";
+import BelowIcon from "../../../icons/BelowIcon";
+import CalendarIcon from "../../../icons/CalendarIcon";
+import DeleteIcon from "../../../icons/DeleteIcon";
+import DuplicateIcon from "../../../icons/DuplicateIcon";
+import EditIcon from "../../../icons/EditIcon";
+import EmailIcon from "../../../icons/EmailIcon";
+import FavoriteHeartIcon from "../../../icons/FavoriteHeartIcon";
+import ShareIcon from "../../../icons/ShareIcon";
 
 interface ThreeDotComponentProps {
   children: ReactNode; // specify the type of children
@@ -18,6 +28,49 @@ interface ThreeDotComponentProps {
 
 const ThreeDotComponent = ({ children }: ThreeDotComponentProps) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const ThreeDotData = [
+    {
+      Icon: AboveIcon,
+      text: "Add project above"
+    },
+    {
+      Icon: BelowIcon,
+      text: "Add project below"
+    },
+    {
+      Icon: EditIcon,
+      text: "Edit project"
+    },
+    {
+      Icon: ShareIcon,
+      text: "Share project"
+    },
+    {
+      Icon: FavoriteHeartIcon,
+      text: "Add to favorites"
+    },
+    {
+      Icon: DuplicateIcon,
+      text: "Duplicate project"
+    },
+    {
+      Icon: EmailIcon,
+      text: "Email tasks to this project"
+    },
+    {
+      Icon: CalendarIcon,
+      text: "Project calendar feed"
+    },
+    {
+      Icon: ArchiveIcon,
+      text: "Archive project"
+    },
+    {
+      Icon: DeleteIcon,
+      text: "Delete project"
+    }
+  ];
+
   return (
     <>
       <Modal
@@ -32,9 +85,6 @@ const ThreeDotComponent = ({ children }: ThreeDotComponentProps) => {
           <ModalCloseButton />
           <ModalBody></ModalBody>
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Close
-            </Button>
             <Button variant="ghost">Secondary Action</Button>
           </ModalFooter>
         </ModalContent>
