c6c1247d246dc7c5fd1848c6888bf20e802657f9
ahmet <ahmet55uluisik05@gmail.com>
2023-05-10 21:19:51 +0300

    changed icons

--- src/pages/FiltersAndLabels/components/ItemCard.tsx
+++ src/pages/FiltersAndLabels/components/ItemCard.tsx
@@ -3,7 +3,6 @@ import { Flex, Spacer, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { PATHS } from "../../../utils/paths";
 import IconsBar from "./IconsBars";
-import DeleteItemModal from "../../../modals/DeleteItemModal";
 
 type ItemCardProps = {
   id: string;
@@ -13,13 +12,7 @@ type ItemCardProps = {
   color: string;
 };
 
-const ItemCard: React.FC<ItemCardProps> = ({
-  id,
-  text,
-  Icon,
-  tooltipLabel,
-  color
-}) => {
+const ItemCard: React.FC<ItemCardProps> = ({ id, text, Icon, color }) => {
   const navigate = useNavigate();
 
   const handleItemClick = () => {
@@ -43,19 +36,6 @@ const ItemCard: React.FC<ItemCardProps> = ({
       </Flex>
       <Spacer />
       <IconsBar />
-      <Flex
-        w="24px"
-        h="24px"
-        alignItems="center"
-        justifyContent="center"
-        ml="1"
-        color="gray"
-        _hover={{ color: "#202020", bg: "#eee" }}
-        borderRadius="20%"
-        fontSize="2xl"
-      >
-        <DeleteItemModal tooltipLabel={tooltipLabel} text={text} id={id} />
-      </Flex>
     </Flex>
   );
 };
