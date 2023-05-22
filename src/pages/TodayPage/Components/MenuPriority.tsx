fb0e7afc14d95531fee3de1c565a34988bd15b9a
h-bura <hsyn.bura@gmail.com>
2023-05-22 21:19:52 +0300

    add features to Priority

--- src/pages/TodayPage/Components/MenuPriority.tsx
+++ src/pages/TodayPage/Components/MenuPriority.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Menu,
   MenuButton,
@@ -11,32 +12,34 @@ import ColorFlagIcon from "../../../icons/ColorFlagIcon";
 import SmallFlag from "../../../icons/SmallFlag";
 import TickIcon from "../../../icons/TickIcon";
 import WhiteFlagIcon from "../../../icons/WhiteFlagIcon";
+import { useState } from "react";
+import DeleteIcon from "../../../icons/DeleteIcon";
 
 const menuItemsPriority = [
   {
-    icon: <ColorFlagIcon color="red" />,
-    text: "Priority 1"
+    number: "P1",
+    color: "red"
   },
   {
-    icon: <ColorFlagIcon color="orange" />,
-    text: "Priority 2"
+    number: "P2",
+    color: "orange"
   },
   {
-    icon: <ColorFlagIcon color="blue" />,
-    text: "Priority 3"
-  },
-  {
-    icon: <WhiteFlagIcon />,
-    text: "Priority 4",
-    icon2: <TickIcon color="red" />
+    number: "P3",
+    color: "blue"
   }
 ];
 
 function MenuPriority() {
+  const [selectedPriority, setSelectedPriority] = useState({
+    number: "",
+    color: ""
+  });
   return (
     <Menu>
       <Tooltip hasArrow label="Set priority p1, p2, p3, p4" placement="top">
         <MenuButton
+          _hover={{ bg: "#eee" }}
           as={Button}
           bg="white"
           border="1px"
@@ -46,24 +49,69 @@ function MenuPriority() {
           h="32px"
           mr="2"
           color="gray"
-          p="0 0 0 1"
-          leftIcon={<SmallFlag fontSize="sm" />}
+          pl="1"
+          leftIcon={<SmallFlag color={selectedPriority.color} />}
+          rightIcon={
+            selectedPriority.number != "" ? (
+              <Box
+                onClick={(e) => {
+                  e.stopPropagation(),
+                    setSelectedPriority({
+                      number: "",
+                      color: ""
+                    });
+                }}
+                bg="transparent"
+                _hover={{ bg: "#d3d3d3" }}
+                borderRadius="5px"
+              >
+                <DeleteIcon />
+              </Box>
+            ) : undefined
+          }
         >
-          <Text mr="2" fontSize="sm">
-            Priority
+          <Text fontSize="sm" display="inline-block">
+            {selectedPriority.number === ""
+              ? "Priority"
+              : selectedPriority.number}
           </Text>
         </MenuButton>
       </Tooltip>
       <MenuList p="0" minWidth="150px" overflowY="scroll">
         {menuItemsPriority.map((item) => (
-          <MenuItem key={item.text}>
-            {item.icon}
+          <MenuItem
+            key={item.number}
+            onClick={() =>
+              setSelectedPriority({
+                number: item.number,
+                color: item.color
+              })
+            }
+          >
+            <ColorFlagIcon color={item.color} />
             <Text ml="2" mr="2">
-              {item.text}
+              Priority {item.number.slice(-1)}
             </Text>
-            {item.icon2}
+            {selectedPriority.number === item.number && (
+              <TickIcon color="red" />
+            )}
           </MenuItem>
         ))}
+        <MenuItem
+          onClick={() =>
+            setSelectedPriority({
+              number: "",
+              color: ""
+            })
+          }
+        >
+          <WhiteFlagIcon />
+          <Text ml="2" mr="2">
+            {" "}
+            Priority 4
+          </Text>
+          {selectedPriority.number === "" && <TickIcon color="red" />}
+        </MenuItem>
       </MenuList>
     </Menu>
   );
