60563efe9d93c16bce186fb1db46325d6da54a8e
h-bura <hsyn.bura@gmail.com>
2023-06-05 16:34:23 +0300

    fix MenuToday

--- src/pages/TodayPage/Components/MenuToday.tsx
+++ src/pages/TodayPage/Components/MenuToday.tsx
@@ -27,12 +27,9 @@ const DATE_OPTIONS = {
   NEXT_WEEK: "Next weekend",
   NO_DATE: "No date"
 };
-
 function MenuToday() {
   const date = new Date(Date.now());
-
   const [startDate, setStartDate] = useState(date);
-
   const weeksDate = new Date(Date.now() + 3600 * 1000 * 24);
   const nextWeeks = new Date(Date.now() + 3600 * 1000 * 24 * 7);
   const formatOptions: Intl.DateTimeFormatOptions = {
@@ -44,29 +41,6 @@ function MenuToday() {
   const tomorrow = weeksDate.toLocaleDateString("en-US", formatOptions);
   const nextWeek = nextWeeks.toLocaleDateString("en-US", formatOptions);
   const [checkedDate, setCheckedDate] = useState(today);
-  const menuItemsToday = [
-    {
-      icon: <TodayIcon width="24" height="18" color="green" />,
-      text1: DATE_OPTIONS.TODAY,
-      text2: today.slice(0, 3)
-    },
-    {
-      icon: <SunIcon color="orange" />,
-      text1: DATE_OPTIONS.TOMORROW,
-      text2: tomorrow.slice(0, 3)
-    },
-
-    {
-      icon: <CouchIcon color="#246fe0" />,
-      text1: DATE_OPTIONS.NEXT_WEEK,
-      text2: nextWeek
-    },
-    {
-      icon: <UpsentIcon />,
-      text1: DATE_OPTIONS.NO_DATE
-    }
-  ];
-
   return (
     <Menu>
       <Tooltip hasArrow label="Set due date" placement="top">
@@ -104,12 +78,12 @@ function MenuToday() {
             placeholder="Due date"
           >
             {checkedDate === today
-              ? "Today"
+              ? DATE_OPTIONS.TODAY
               : checkedDate === tomorrow
-              ? "Tomorrow"
+              ? DATE_OPTIONS.TOMORROW
               : checkedDate
               ? checkedDate.slice(4)
-              : "Due date"}
+              : DATE_OPTIONS.NO_DATE}
           </Text>
         </MenuButton>
       </Tooltip>
@@ -119,56 +93,71 @@ function MenuToday() {
             <Text>{checkedDate.slice(4)}</Text>
           </MenuItem>
           <MenuDivider />
-          {menuItemsToday.map((item) => (
-            <MenuItem
-              display={
-                (item.text1 === "Today" && checkedDate === today) ||
-                (item.text1 === "Tomorrow" && checkedDate === tomorrow)
-                  ? "none"
-                  : "flex"
-              }
-              fontSize="13px"
-              fontWeight="500"
-              key={item.text1}
-              onClick={() => {
-                if (item.text1 === "Tomorrow") {
-                  setCheckedDate(tomorrow);
-                } else if (item.text1 === "Today") {
-                  setCheckedDate(today);
-                } else if (item.text1 === "Next weekend") {
-                  setCheckedDate(nextWeek);
-                } else if (item.text1 === "No date") {
-                  setCheckedDate("");
-                }
-              }}
-            >
-              {item.icon}
-              <Text ml="2">{item.text1}</Text>
-              <Spacer />
-              <Text color="#808080" fontWeight="400">
-                {item.text2}
-              </Text>
-            </MenuItem>
-          ))}
+          <MenuItem
+            onClick={() => setCheckedDate(today)}
+            display={checkedDate === today ? "none" : "flex"}
+            fontSize="13px"
+            fontWeight="500"
+          >
+            <TodayIcon width="24" height="18" color="green" />
+            <Text ml="2">{DATE_OPTIONS.TODAY}</Text>
+            <Spacer />
+            <Text color="#808080" fontWeight="400">
+              {today.slice(0, 3)}
+            </Text>
+          </MenuItem>
+          <MenuItem
+            onClick={() => setCheckedDate(tomorrow)}
+            display={checkedDate === tomorrow ? "none" : "flex"}
+            fontSize="13px"
+            fontWeight="500"
+          >
+            <SunIcon color="orange" />
+            <Text ml="2">{DATE_OPTIONS.TOMORROW}</Text>
+            <Spacer />
+            <Text color="#808080" fontWeight="400">
+              {tomorrow.slice(0, 3)}
+            </Text>
+          </MenuItem>
+          <MenuItem
+            onClick={() => setCheckedDate(nextWeek)}
+            fontSize="13px"
+            fontWeight="500"
+          >
+            <CouchIcon color="#246fe0" />
+            <Text ml="2">{DATE_OPTIONS.NEXT_WEEK}</Text>
+            <Spacer />
+            <Text color="#808080" fontWeight="400">
+              {nextWeek}
+            </Text>
+          </MenuItem>
+          <MenuItem
+            onClick={() => setCheckedDate("")}
+            fontSize="13px"
+            fontWeight="500"
+          >
+            <UpsentIcon />
+            <Text ml="2">{DATE_OPTIONS.NO_DATE}</Text>
+          </MenuItem>
           <MenuDivider />
+          <Flex ml="15px" alignItems="center">
+            <TodayIcon />
+            <Box ml="10px">
+              <ReactDatePicker
+                selected={startDate}
+                onChange={(date: Date) => {
+                  setStartDate(date),
+                    setCheckedDate(
+                      date.toLocaleDateString("en-US", formatOptions)
+                    );
+                }}
+                showTimeSelect
+                dateFormat="d MMMM, yyyy h:mm aa"
+                minDate={new Date()}
+              />
+            </Box>
+          </Flex>
         </MenuGroup>
-        <Flex ml="15px" alignItems="center">
-          <TodayIcon />
-          <Box ml="10px">
-            <ReactDatePicker
-              selected={startDate}
-              onChange={(date: Date) => {
-                setStartDate(date),
-                  setCheckedDate(
-                    startDate.toLocaleDateString("en-US", formatOptions)
-                  );
-              }}
-              showTimeSelect
-              dateFormat="d MMMM, yyyy h:mm aa"
-              minDate={new Date()}
-            />
-          </Box>
-        </Flex>
       </MenuList>
     </Menu>
   );
